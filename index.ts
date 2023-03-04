c4b4b0348ac872e00f46e440f34a1f810dbfcc8b
Botshxlo <46230844+Botshxlo@users.noreply.github.com>
2023-03-04 03:02:42 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 // Import stylesheets
 import './style.css';
+import { parse } from 'papaparse';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-fetch("www.google.com").then((res => console.log(res.json())))
+fetch('Book1.xlsx')
+  .then((response) => response.text())
+  .then((csv) => {
+    const results = parse(csv, { header: true });
+    console.log(results.data); // This will output an array of objects representing the data in the CSV file
+  });
